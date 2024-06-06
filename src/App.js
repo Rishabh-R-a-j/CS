9d34a146f4d650bc3f64c8298fb2751e6faf5fdc
Rishabh Raj <rishabhraj1976@gmail.com>
2024-06-06 17:57:54 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './css/props.css';
 import './css/uifont.css';
 import HomePage from './screens/Home';
 import Sidebar from './screens/Sidebar';
+import Rightbar from './screens/Rightbar';
 function App() {
   return (
     <div className="App flex">
       <Sidebar/>
+      <div className="app-content">
       <HomePage/>
+      </div>
+      <Rightbar/>
     </div>
   );
 }
